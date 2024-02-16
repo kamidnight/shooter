17d26a86e2bf969d029f742c0fc6f8ef074a28c9
kamidnight <mimidnight.mia@gmail.com>
2024-02-16 22:53:08 +0300

    camera cpntroller animation

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,33 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float gravity = 9.8f;
-    public float jumpForce;
+    public float Gravity = 9.8f;
+    public float JumpForce = 7;
+    public float Speed = 5;
 
-    private float _fallVelocity = 0; 
+    private Vector3 _moveVector;
+    private float _fallVelocity = 0;
 
     private CharacterController _characterController;
-
-    // Start is called before the first frame update
+  
     void Start()
     {
-         _characterController = GetComponent<CharacterController>();
+        _characterController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
         {
-            _fallVelocity = -jumpForce;
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -JumpForce;
         }
     }
-    // Update is called once per frame
+
     void FixedUpdate()
     {
-        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(_moveVector * Speed * Time.fixedDeltaTime);
+
+        _fallVelocity += Gravity * Time.fixedDeltaTime;
         _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if(_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
     }
 }
