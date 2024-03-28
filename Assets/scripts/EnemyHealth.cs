c34903fb75e961e5d53ae0e7475598dc1f3c8558
kamidnight <mimidnight.mia@gmail.com>
2024-03-28 21:20:10 +0300

    konets

--- Assets/scripts/EnemyHealth.cs
+++ Assets/scripts/EnemyHealth.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float value = 100;
+    public Animator animator;
 
     public void DealDamage(float damage)
     {
         value -= damage;
         if (value <= 0)
         {
-            Destroy(gameObject);
+            EnemyDeath();
+        }
+        else
+        {
+            animator.SetTrigger("hit");
         }
     }
     // Start is called before the first frame update
-    void Start()
+    private void EnemyDeath()
     {
-        
+        animator.SetTrigger("death");
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
